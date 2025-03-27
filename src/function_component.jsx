2cc71dd5094ade9bb2062b8322c65209c066c98d
ebra2022-sudo <ebrahimmuhammed479@gmail.com>
2025-03-27 17:40:32 +0300

    Define state and manage the state in the function component

--- src/function_component.jsx
+++ src/function_component.jsx
@@ -1,8 +1,16 @@
+import { useState } from "react";
 
 function FunctionCompnent() {
+    const [count1, setCount1] = useState(0);
+    const [count2, setCount2] = useState(0);
+    
     return (
         <div>
-            This is ui from function component
+            <h1>This is UI from the function component</h1>
+            <h5>Counter1 = {count1}</h5>
+            <h5>Counter2 = {count2}</h5>
+            <button onClick={() => setCount1(count1 + 1)}>Increment counter 1</button>
+            <button onClick={() => setCount2(count2 + 1)}>Increment counter 2</button>
         </div>
     );
 }
